b5990d53ce547cb74271ac28b14e703f89a35710
Mary Grace Lucas <graceylucas@hotmail.com>
2016-02-08 20:33:21 -0500

    Outlines joke/key properties and user properties

--- FirebaseJokes/AddJokeViewController.swift
+++ FirebaseJokes/AddJokeViewController.swift
@@ -8,15 +8,33 @@
 
 import UIKit
 import Foundation
+import Firebase
 
 class AddJokeViewController: UIViewController {
     
+    
+    var currentUsername:String = ""
+
+    
     @IBOutlet weak var jokeField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Get username of person adding joke
+        DataService.dataService.CURRENT_USER_REF.observeEventType(FEventType.Value , withBlock: {  (snapshot) -> Void in
+            let currentUser = snapshot.value.objectForKey("username") as! String
+            
+            print("Username: \(currentUser)")
+            self.currentUsername = currentUser
+            
+        }) { error in
+            print(error.description)
+        }
+        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
--- FirebaseJokes/Joke.swift
+++ FirebaseJokes/Joke.swift
@@ -9,3 +9,61 @@
 import Foundation
 import Firebase
 
+class Joke {
+    
+    private var _jokeRef: Firebase!
+    
+    // Creates private variables for joke ID and properties
+    private var _jokeKey: String!
+    private var _jokeText: String!
+    private var _jokeVotes: Int!
+    private var _username: String!
+    
+    // Public version of the above variables
+    var jokeKey: String {
+        return _jokeKey
+    }
+    
+    var jokeText: String {
+        return _jokeText
+    }
+    
+    var jokeVotes: Int {
+        return _jokeVotes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    
+    // Initializes a joke (key)
+    
+    init(key: String, dictionary: Dictionary<String, AnyObject>) {
+        self._jokeKey = key
+        
+        
+        // Within the Joke, or Key, the following properties are children
+        
+        if let votes = dictionary["votes"] as? Int {
+            self._jokeVotes = votes
+        }
+        
+        if let joke = dictionary["jokeText"] as? String {
+            self._jokeText = joke
+        }
+        
+        if let user = dictionary["author"] as? String {
+            self._username = user
+            
+        } else {
+            
+            self._username = ""
+        }
+        
+        // The above properties are assigned to their key.
+        
+        self._jokeRef = DataService.dataService.JOKE_REF.childByAppendingPath(self._jokeKey)
+    }
+}
+    
